c1808f88fb2cdde93b6775456683b85a1b492b3e
aviad <aviadhadad@gmail.com>
2014-07-03 16:35:36 +0300

    small fix to run file

--- src/RunFromTextFile.java
+++ src/RunFromTextFile.java
@@ -10,11 +10,23 @@ public class RunFromTextFile {
 	
 	System.out.println("Loading the Graph");
 	Graph graph = Graph.FromMatrix(n, filename);
-	System.out.println("Pringing the Graph");
-	System.out.println(graph.toString());
-	
+	System.out.println("Running Floyd-Marshell");
+	int dist[][] = graph.Floyd_Warshall();
+	System.out.println("Generating Spanner");
 	Graph spanner = graph.MakeRSpanner(r);
 	spanner.toCSV(args[3]+"-edges.csv", args[3]+"-nodes.csv");
+	System.out.println("Running FloydMarshell on spanner");
+	int dist2[][] = spanner.Floyd_Warshall();
+	System.out.println("Verifying Distances are no larger than parameter r="+r);
+	int size = dist.length;
+	for (int i=0;i<size;i++) 
+	    for (int j=0;j<size;j++)
+		if (dist[i][j]*r<dist2[i][j])
+		    System.out.println("Found ERROR! spanner doesnt hold the requirements! "+i+"->"+j);
+	System.out.println("Number of edges in original Graph:"+graph.edges.size()/2);
+	System.out.println("Number of edges in r-spanner Graph:"+spanner.edges.size()/2);
+	System.out.println("Finished!");
+	
 	
     }
     
